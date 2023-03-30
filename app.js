3aa7b9cc5137b4ea9116cb9a33f0b5b643c6b11c
VINICIUS NUNES DE OLIVEIRA <22282114@CFP127ALUNO.LOCAL>
2023-03-30 11:20:12 -0300

    back em progesso

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use((request, response, next) => {
   next()
 })
 
+//endpoints
+
 app.get("/v1/lion-school/cursos", cors(), async function (request, response, next) {
   
     let cursos = model.getCursos()
@@ -33,18 +35,60 @@ app.get("/v1/lion-school/cursos", cors(), async function (request, response, nex
 })
 
 app.get("/v1/lion-school/alunos", cors(), async function (request, response, next) {
-  
-    let alunos = model.getTodosAlunos()
 
+    let alunos = model.getTodosAlunos()
+    let siglaCurso = request.query.curso
+    let statusAluno = request.query.status
+    let dadosAluno = {}
+
+
+    if(siglaCurso == undefined && statusAluno == undefined){
     if(alunos){
       response.json(alunos);
       response.status(200);
     }else{
       response.status(500)
+    }}
+    else if (siglaCurso != undefined){
+      if(siglaCurso == '' || siglaCurso == undefined || !isNaN(siglaCurso)){
+        statusCode = 400
+        dadosAluno.message =  "Não é possível processar a requisição, pois a sigla do curso não foi informada ou não é válida."
+      }else{
+        let aluno = model.getAlunosCurso(siglaCurso)
+          if(aluno){
+            statusCode = 200
+            dadosAluno = aluno
+          }else{
+            statusCode = 404
+          }
+      }
+      response.status(statusCode)
+      response.json(dadosAluno)
+    }else if (statusAluno != undefined) {
+      if(statusAluno == '' || statusAluno == undefined || !isNaN(statusAluno)){
+        statusCode = 400
+        dadosAluno.message =  "Não é possível processar a requisição, pois a sigla do curso não foi informada ou não é válida."
+      }else{
+        let aluno = model.getStatusAluno(statusAluno)
+          if(aluno){
+            statusCode = 200
+            dadosAluno = aluno
+          }else{
+            statusCode = 404
+          }
+      }
+      response.status(statusCode)
+      response.json(dadosAluno)
+    }else{
+      statusCode = 400
+      dadosAluno.message =  "Não é possível processar a requisição, verifique a URL da requisição."
+      response.status(statusCode)
+      response.json(dadosAluno)
     }
+  
 })
 
-app.get("/v1/lion-school/alunos/:matricula", cors(), async function( request, response, next){
+app.get("/v1/lion-school/alunos/:matricula", cors(), async function(request, response, next){
     // '/:uf' é uma variavel utilizada para passagem de valor
   
     // siglaEstado recebe o conteudo da variavel :uf
@@ -68,11 +112,9 @@ app.get("/v1/lion-school/alunos/:matricula", cors(), async function( request, re
     response.json(dadosAluno)
   })
 
-  app.get("/v1/lion-school/alunos-curso/:curso", cors(), async function( request, response, next){
-    // '/:uf' é uma variavel utilizada para passagem de valor
+  app.get("/v1/lion-school/alunos", cors(), async function(request, response, next){
   
-    // siglaEstado recebe o conteudo da variavel :uf
-    let siglaCurso = request.params.curso
+    let siglaCurso = request.query.curso
     let statusCode
     let dadosAluno = {}
   
@@ -92,6 +134,30 @@ app.get("/v1/lion-school/alunos/:matricula", cors(), async function( request, re
     response.json(dadosAluno)
   })
 
+  app.get("/v1/lion-school/alunos", cors(), async function(request, response, next){
+  
+    let statusAluno = request.query.status
+    let statusCode
+    let dadosAluno = {}
+  
+    if(statusAluno == '' || statusAluno == undefined || !isNaN(statusAluno)){
+      statusCode = 400
+      dadosAluno.message =  "Não é possível processar a requisição, pois a sigla do curso não foi informada ou não é válida."
+    }else{
+      let aluno = model.getStatusAluno(statusAluno)
+        if(aluno){
+          statusCode = 200
+          dadosAluno = aluno
+        }else{
+          statusCode = 404
+        }
+    }
+    response.status(statusCode)
+    response.json(dadosAluno)
+  })
+
+
+  //indica a porta
 
 app.listen(8080, function () {
     console.log("Servidor aguardando requisições");
