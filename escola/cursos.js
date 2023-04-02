3a3c8a5c663f9601c128445b6bb3badb9426cb6b
VINICIUSNUNES137 <90266473+VINICIUSNUNES137@users.noreply.github.com>
2023-04-02 00:48:35 -0300

    imagem alterada

--- escola/cursos.js
+++ escola/cursos.js
@@ -1,15 +1,31 @@
 var cursos = [
     {
-        "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
-        "sigla" :   "DS",
-        "icone" :   "https://image.shutterstock.com/image-vector/api-interface-vector-icon-600w-659203513.jpg",
-        "carga" :   "1200",
+        "nome": "001 - Técnico em Desenvolvimento de Sistemas",
+        "sigla": "DS",
+        "icone": "https://raw.githubusercontent.com/VINICIUSNUNES137/lion-school_front/main/img/ds.svg",
+        "carga": "1200",
     },
     {
-        "nome"  :   "002 - Técnico em Redes de Computadores",
-        "sigla" :   "RDS",
-        "icone" :   "https://img.icons8.com/ultraviolet/344/thin-client.png",
-        "carga" :   "1200"
+        "nome": "002 - Técnico em Redes de Computadores",
+        "sigla": "RDS",
+        "icone": "https://raw.githubusercontent.com/VINICIUSNUNES137/lion-school_front/main/img/rds.svg",
+        "carga": "1200"
+    },
+    {
+        "nome": "002 - Técnico em Redes de Computadores",
+        "sigla": "RDS",
+        "icone": "https://raw.githubusercontent.com/VINICIUSNUNES137/lion-school_front/main/img/rds.svg",
+        "carga": "1200"
+    }, {
+        "nome": "001 - Técnico em Desenvolvimento de Sistemas",
+        "sigla": "DS",
+        "icone": "https://raw.githubusercontent.com/VINICIUSNUNES137/lion-school_front/main/img/ds.svg",
+        "carga": "1200",
+    }, {
+        "nome": "001 - Técnico em Desenvolvimento de Sistemas",
+        "sigla": "DS",
+        "icone": "https://raw.githubusercontent.com/VINICIUSNUNES137/lion-school_front/main/img/ds.svg",
+        "carga": "1200",
     }
 ];
 
